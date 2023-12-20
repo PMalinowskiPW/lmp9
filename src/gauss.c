0b03057fccbea9122a46b2ba8877b9032d970e23
PMalinowskiPW <pawel8670@gmail.com>
2023-12-20 18:17:52 +0100

    dodanie kodu metody gaussa

--- src/gauss.c
+++ src/gauss.c
@@ -1,14 +1,30 @@
+
 #include "gauss.h"
+#include "mat_io.h"
+#include <stdio.h>
 
 /**
  * Zwraca 0 - elimnacja zakonczona sukcesem
  * Zwraca 1 - macierz osobliwa - dzielenie przez 0
  */
 int eliminate(Matrix *mat, Matrix *b){
-    /**
-  	 * Tutaj należy umieścić właściwą implemntację.
-		 */
+for (int k=0; k < mat->r; k++){
+		eliminacja(k,mat,b);	
+		}
+return 0;
+}
 
-		return 0;
+
+void eliminacja(int k,Matrix *mat, Matrix *b){
+	for (int i = k+1; i < mat->r; i++)
+        	if (mat->data[i][k] != 0){
+                	double i_0 =  mat->data[i][k];
+                	b->data[i][0] = (b->data[i][0] * mat->data[k][k] / i_0) - b->data[k][0];
+                        for (int j = k; j < mat->c; j++){
+                        	mat->data[i][j] = (mat->data[i][j] * mat->data[k][k] / i_0) - mat->data[k][j];
+
+                        }
+             	}
+
 }
 
--- src/gauss.h
+++ src/gauss.h
@@ -2,11 +2,11 @@
 #define _GAUSS_H
 
 #include "mat_io.h"
-
+#include <stdio.h>
 /**
  * Zwraca 0 - elimnacja zakonczona sukcesem
  * Zwraca 1 - macierz osobliwa - dzielenie przez 0
  */
 int eliminate(Matrix *mat, Matrix *b);
-
+void eliminacja(int k, Matrix *mat , Matrix *b);
 #endif
